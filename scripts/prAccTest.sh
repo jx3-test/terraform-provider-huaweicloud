09369b9a1f017e9f53bcf04c81cfa1c7704f7a0e
hua ming <hmbig2@163.com>
2022-04-16 10:50:55 +0800

    test: add a acc test to pr

---
+++ scripts/prAccTest.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#git fetch --no-tags --prune --depth=1 origin +refs/heads/master:refs/remotes/origin/master
+all_files=$(git diff origin/master HEAD --name-only huaweicloud | grep -v "_test.go")
+
+for f in $all_files; do
+    path=${f%/*}
+    if [ "$path" != "huaweicloud" ]; then
+        # update path to "huaweicloud/services/acceptance/xxx"
+        path=${path/services/services\/acceptance}
+    fi
+
+    org_file=${f##*/}
+    test_file=$path/${org_file/%.go/_test.go}
+
+    if [ -f "./${test_file}" ]; then
+        basic_cases=$(grep "^func TestAcc" ./${test_file} | grep _basic | awk '{print $2}' | awk -F '(' '{print $1}')
+        if [ "X$basic_cases" != "X" ]; then
+            echo -e "\n\`\`\` \nrun acceptance basic tests: $basic_cases"
+            make testacc TEST="./$path" TESTARGS="-run ${basic_cases}"
+            echo "\`\`\`"
+        fi
+    else
+        echo -e "[skipped] --- ./${test_file} does not exist"
+    fi
+done
